7c15f4002c336a6fdafdbed2ff501c8aed6a1ab5
Brijanshu Chauhan <brijanshuchauhan7011@gmail.com>
2024-09-06 17:37:36 +0530

    added wrong inpout

--- script.js
+++ script.js
@@ -4,20 +4,32 @@ let btn=document.querySelector(".button-ele");
 const WIcon=document.querySelector(".weather-icon");
 let city=document.querySelector(".input-city");
 const card=document.querySelector(".weather");
+const details=document.querySelector(".details");
 
 async function checkweather(City) {
     url=`https://api.openweathermap.org/data/2.5/weather?q=${City}&appid=${apiKey}&units=metric`
     const response = await fetch(url);
     const data= await response.json();
     card.classList.remove("remove");
-    card.style.animationDuration="2s";
-    document.querySelector(".city").innerHTML=data.name;
-    document.querySelector(".temp").innerHTML=data.main.temp;
-    document.querySelector(".humidity").innerHTML=data.main.humidity;
-    document.querySelector(".wind").innerHTML=data.wind.speed;
-    W_main=data.weather[0].main;
-    WIcon.src=`images/${W_main}.png`
+    //card.style.animationDuration="2s";
+    if(data.message==="city not found"){
+        details.classList.add("remove");
+        document.querySelector(".city").innerHTML="Please Enter correct City";
+        document.querySelector(".temp").innerHTML=" ";
+        document.querySelector(".humidity").innerHTML=" ";
+        document.querySelector(".wind").innerHTML=" ";
+        WIcon.src=`images/error.png`
+    }
+    else{
+        details.classList.remove("remove");
+        document.querySelector(".city").innerHTML=data.name;
+        document.querySelector(".temp").innerHTML=data.main.temp;
+        document.querySelector(".humidity").innerHTML=data.main.humidity;
+        document.querySelector(".wind").innerHTML=data.wind.speed;
+        W_main=data.weather[0].main;
+        WIcon.src=`images/${W_main}.png`
  }
+}
 
  
  btn.addEventListener("click",run=()=>{
